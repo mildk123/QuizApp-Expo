b4c73fb46000a90106e945fbed411af9d69a4d51
mildk123 <mildk123@gmail.com>
2018-11-22 08:55:09 +0500

    Added Splashscreen

--- navigation/Router.js
+++ navigation/Router.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 
-import BackDrop from '../screens/BackDrop'
+// import BackDrop from '../screens/BackDrop'
 import MainTabNavigator from './MainTabNavigator';
 import Questions from '../screens/Questions';
 
 export default createStackNavigator({
-    BackDrop: BackDrop,
+    // BackDrop: BackDrop,
     Main: MainTabNavigator,
     Questions: Questions,
 });
